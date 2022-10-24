12297baedfc5a4a416d84e462b4cf825b2dd07c8
omarwebdeve <omarf977865@gmail.com>
2022-10-24 08:48:18 +0600

    home section

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,15 +2,75 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='bg-black'>
-            <div className='opacity-30 grid grid-cols-2 justify-center items-center  bg-no-repeat bg-cover no-react bg-[url("https://i.ibb.co/fkP9kPs/Rectangle-1.png")] h-[100vh] w-full'>
-                <div className=''>
-                    <h2 className='z-50 text-opacity-90 text-white text-[97.68px] '>Cox's bazar</h2>
+        <div className=' bg-no-repeat bg-cover no-react bg-[url("https://i.ibb.co/fkP9kPs/Rectangle-1.png")]'>
+            <div className='my-bg-image relative grid grid-cols-2 justify-center h-[100vh] items-center bg-opacity-70 bg-black' >
+                <div className='pr-[10%] pl-[15%] '>
+                    <h2 className='  text-white text-[97.68px] '>Cox's bazar</h2>
+                    <p className='text-white'>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                    <button className='btn btn-sm font-semibold my-3 rounded-sm bg-[#F9A51A] text-black'>Get Start</button>
                 </div>
+
                 <div>
-                    omar
+                    <section class=" mx-auto px-4 sm:px-6 lg:px-4 mt-12 mb-12">
+                        <article>
+                            <section class="mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
+                                <article class="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+                                    <div class="relative w-full h-80 md:h-64 lg:h-44">
+                                        <img src="https://cdn.pixabay.com/photo/2021/07/24/01/42/zebra-dove-6488440_960_720.jpg"
+                                            alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                                            class="w-full h-full object-center object-cover"/>
+                                    </div>
+                                    <div class="px-3 py-4">
+                                        <h3 class="text-sm text-gray-500 pb-2">
+                                            <a class="bg-indigo-600 py-1 px-2 text-white rounded-lg" href="#">
+                                                <span class="absolute inset-0"></span>
+                                                Cox's bazar
+                                            </a>
+                                        </h3>
+                                        <p class="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    </div>
+                                </article>
+                               
+                                <article class="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+                                    <div class="relative w-full h-80 md:h-64 lg:h-44">
+                                        <img src="https://cdn.pixabay.com/photo/2021/08/03/11/01/stairs-6519085_960_720.jpg"
+                                            alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                                            class="w-full h-full object-center object-cover"/>
+                                    </div>
+                                    <div class="px-3 py-4">
+                                        <h3 class="text-sm text-gray-500 pb-2">
+                                            <a class="bg-indigo-600 py-1 px-2 text-white rounded-lg" href="#">
+                                                <span class="absolute inset-0"></span>
+                                                Bander Ban
+                                            </a>
+                                        </h3>
+                                        <p class="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    </div>
+                                </article>
+                                <article class="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+                                    <div class="relative w-full h-80 md:h-64 lg:h-44">
+                                        <img src="https://cdn.pixabay.com/photo/2021/09/12/17/43/parrot-feathers-6619082_960_720.jpg"
+                                            alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                                            class="w-full h-full object-center object-cover"/>
+                                    </div>
+                                    <div class="px-3 py-4">
+                                        <h3 class="text-sm text-gray-500 pb-2">
+                                            <a class="bg-indigo-600 py-1 px-2 text-white rounded-lg" href="#">
+                                                <span class="absolute inset-0"></span>
+                                                Nijom Drip
+                                            </a>
+                                        </h3>
+                                        <p class="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                                            Noakhali Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    </div>
+                                </article>
+                            </section>
+                        </article>
+                    </section>
                 </div>
-                 
+
             </div>
         </div>
     );
--- src/SharePage/Header.jsx
+++ src/SharePage/Header.jsx
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className="absolute bg-white w-full flex justify-between items-center mx-auto px-8 h-20">
+        <nav className="absolute opacity-100 z-50 text-white w-full flex justify-between items-center mx-auto px-8 h-20">
             <div className='flex justify-between'>
-
                 <div className="inline-flex items-center pr-5">
                     <Link className="" to="/" >
                         Brad name
                     </Link>
                 </div>
-
                 <div className="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
                     <div className="block md:inline-block">
                         <div className="inline-flex items-center max-w-full">
-                            <button className="flex items-center flex-grow-0 flex-shrink pl-2 relative w-60 border rounded-full px-1  py-1" type="button">
+                            <button className="flex items-center flex-grow-0 flex-shrink pl-2 relative w-60 border rounded-md px-1  py-1" type="button">
                                 <div className="block flex-grow flex-shrink overflow-hidden">Start your search</div>
-                                <div className="flex items-center justify-center relative  h-8 w-8 rounded-full">
+                                <div className="flex items-center justify-center relative  h-8 w-8 rounded-md">
 
                                 </div>
                             </button>
@@ -29,26 +27,26 @@ const Header = () => {
             <div className="flex-initial">
                 <div className="flex justify-end items-center relative">
                     <div className="block md:flex mr-4 items-center">
-                        <a className="block md:inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">
+                        <a className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" href="#">
                             <div className="flex items-center relative cursor-pointer whitespace-nowrap">Home</div>
                         </a>
-                        <a className="block md:inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">
+                        <a className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" href="#">
                             <div className="flex items-center relative cursor-pointer whitespace-nowrap">Home</div>
                         </a>
-                        <a className="block md:inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">
+                        <a className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" href="#">
                             <div className="flex items-center relative cursor-pointer whitespace-nowrap">Home</div>
                         </a>
-                        <a className="block md:inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">
+                        <a className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" href="#">
                             <div className="flex items-center relative cursor-pointer whitespace-nowrap">Home</div>
                         </a>
-                        <a className="block md:inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">
+                        <a className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" href="#">
                             <div className="flex items-center relative cursor-pointer whitespace-nowrap">Home</div>
                         </a>
                     </div>
                     {/*         
                 <div className="block">
                     <div className="inline relative">
-                        <button type="button" className="inline-flex items-center relative px-2 border rounded-full hover:shadow-lg">
+                        <button type="button" className="inline-flex items-center relative px-2 border rounded-md hover:shadow-lg">
                             <div className="pl-1">
                                
                             </div>
